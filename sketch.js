3f72fb772dbcc960bac9896c2b4e45521b8a7289
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-11-11 22:54:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,16 @@
 
-
-var box1, box2, box3, ground;
-var ball;
-
+var box1;
 
 function setup() 
 {
   createCanvas(1500, 800);
-
-  ground = new Box(0,600,1200,20);
-  box1 = new Box(900, 100, 70, 70);
-  box2 = new Box(800, 100, 70, 70);
-  box3 = new Box(700, 100, 70, 70);
-  
+  box1 = new Box();
 }
 
 function draw() 
 {
   background(220);
-
- 
-  ground.display();
-  
   box1.display();
-  box2.display()
-  box3.display()
-  
-
 }
 
 
